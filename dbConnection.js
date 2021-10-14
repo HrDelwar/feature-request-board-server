c41cb363d665b5b43033ab60f8b6e8c3f905e41e
hrdelwar <hrdelwar75@gmail.com>
2021-10-14 14:52:01 +0600

    congig dotenv

--- dbConnection.js
+++ dbConnection.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const dbConnection = () => {
   const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vzza0.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
   // const uri = `mongodb://localhost:27017/bando-db`
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Feature Request Board Server
+
+## [client code link](https://github.com/HrDelwar/feture-request-board-client)
